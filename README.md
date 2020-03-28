7eaa22474539d90ff1036663f69f0f98b0807265
Lucas Calje <lucas@192.168.2.6>
2020-03-28 16:36:11 +0100

    feat: setup calculation mandelbrot

--- README.md
+++ README.md
@@ -6,4 +6,19 @@ Experimenting with fractals and infinite zooming!
 
 𝑓(𝑧) = 𝑧 <sup>2</sup> + 𝑐
 
+z<sub>n + 1</sub> = z <sup>2</sup><sub>n</sub> + c 
+
+-- 
+
+1) 𝑧 <sub>0</sub> = 0
+2) 𝑧 <sub>1</sub> = 𝑧 <sub>0</sub> + 𝑐
+3) 𝑧 <sub>2</sub> = 𝑧 <sub>1</sub> + 𝑐
+4) etc
+
+# Julia set
+
+# Bookmarks
+
+   * Basics: https://www.youtube.com/watch?v=FFftmWSzgmk 
+   * Approximate PI with Mandelbrot: https://www.youtube.com/watch?v=d0vY0CKYhPY&t=4s
 
--- src/mandelbrot/index.ts
+++ src/mandelbrot/index.ts
@@ -1,5 +1,83 @@
-console.log('Mandelbrot');
 
-const Rmax = 2 
-const Imax = 2 
+import * as fs from 'fs';
 
+console.log('Mandelbrot v1.0.0');
+
+const maxIter = 200;
+
+const maxR = 2;
+const maxI = 2;
+const minR = -2;
+const minI = -2;
+
+const stepsR = 1000;
+const stepsI = 1000;
+const stepSizeR = (maxR - minR) / stepsR;
+const stepSizeI = (maxI - minI) / stepsI;
+
+let zR: number;
+let zI: number;
+
+let zNewR: number;
+let zNewI: number;
+
+let znR: number;
+let znI: number;
+
+// Begin
+zR = 0;
+zI = 0;
+
+const grid: Array<number[]> = [];
+let row: number[];
+
+for (let cR = minR; cR < maxR; cR += stepSizeR) {
+	row = [];
+	for (let cI = minI; cI < maxI; cI += stepSizeI) {
+		row.push(determineStability(zR, zI, cR, cI));
+	}
+	grid.push(row);
+}
+
+writeDataToFile(grid);
+
+// ----------------
+
+
+function determineStability(
+	zR: number, zI: number,
+	cR: number, cI: number,
+	count = 0): number {
+
+	// step 1: is this stable
+	// if bigger than 2 it is considered NOT STABLE
+	if (maxIter === count || Math.sqrt(zR ** 2 + zI ** 2) > 2) {
+		return count;
+	}
+
+	// step 2: compute next value 
+	// z**2 + c = 
+	//            (a + bi)(a + bi) = a**2 - b**2 + 2abi
+	// 			   + 
+	//            c = x + yi
+	//          =  Real: a**2 - b**2 + x
+	//  		   Imag: 2ab + y
+
+	const dumR = zR ** 2 - zI ** 2 + cR;
+	const dumI = 2 * zR * zI + cI;
+
+	return determineStability(dumR, dumI, cR, cI, ++count);
+}
+
+
+function writeDataToFile(grid: Array<number[]>) {
+	fs.writeFile('./mandekbrot.json',
+
+		JSON.stringify(grid),
+
+		(err: any) => {
+			if (err) {
+				console.error('Crap happens', err);
+			}
+		});
+}
